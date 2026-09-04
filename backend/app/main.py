40b023094201f3ba6c7dabd58938883d8eb50302
manisha-2403 <manisharj.24@gmail.com>
2026-09-04 17:12:21 +0530

    Fix CORS for production frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -51,9 +51,9 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173"
-    ],
+    "http://localhost:5173",
+    "https://personal-finance-dashboard-xi-two.vercel.app",
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
